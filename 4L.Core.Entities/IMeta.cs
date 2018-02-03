2134fcb24663ca8daf9798f2b42754b1adea0804
phill.dorrell <phill.dorrell@googlemail.com>
2018-02-03 15:19:46 +0000

    Adding support for ITagName as well as ITag to IMeta

--- 4L.Core.Entities/IMeta.cs
+++ 4L.Core.Entities/IMeta.cs
@@ -17,12 +17,28 @@ namespace _4L.Core.Entities
         /// <param name="tag">The tag we're looking for</param>
         /// <returns>True if it does, otherwise false</returns>
         bool Contains(ITag tag);
+
+        /// <summary>
+        /// Confirms whether this object contains the specified tag name
+        /// </summary>
+        /// <param name="tagName">The tag name we're looking for</param>
+        /// <returns>True if it does, otherwise false</returns>
+        bool Contains(ITagName tagName);
+
         /// <summary>
         /// Determines how closely this object matches the supplied tags
         /// </summary>
         /// <param name="tags">The supplied collection of ITag objects</param>
         /// <returns>A percentage of how many of the supplied tags have been found in this object</returns>
         double Match(IEnumerable<ITag> tags);
+
+        /// <summary>
+        /// Determines how closely this object matches the supplied tag names
+        /// </summary>
+        /// <param name="tagNames">The supplied collection of ITagName objects</param>
+        /// <returns>A percentage of how many of the supplied tag names have been found in this object</returns>
+        double Match(IEnumerable<ITagName> tagNames);
+        
         /// <summary>
         /// Returns a read-only collection of the tags in this object's meta data collection
         /// </summary>
