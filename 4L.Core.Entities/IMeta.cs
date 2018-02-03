e39bf3fe98303307ac626cafb6523c3ccd0ee063
phill.dorrell <phill.dorrell@googlemail.com>
2018-02-03 14:14:55 +0000

    Initial commit of new repository

---
+++ 4L.Core.Entities/IMeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4L.Core.Entities
+{
+    public interface IMeta
+    {
+        void Add(ITag tag);
+        bool Contains(ITag tag);
+        double Match(IEnumerable<ITag> tags);
+        IEnumerable<ITag> Tags { get; }
+    }
+}
---
+++ 4L.Core.Entities/ITag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4L.Core.Entities
+{
+    public interface ITag
+    {
+        string Name { get; set; }
+        string Value { get; set; }
+    }
+}
