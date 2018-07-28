a46dd539472f91aa127c97356b914cb14b76dd1e
phill.dorrell <phill.dorrell@googlemail.com>
2018-07-28 13:35:10 +0100

    Initial version of unit test project

---
+++ 4L.Core.Entities.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace _4L.Core.Entities.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
